3e6bbecff028f0199be3d4db576df007a3f06249
MaYushan <levimacn@qq.com>
2026-01-27 20:30:16 +0800

    Fix Spider Wechat Version

--- spider/apps/wechat.js
+++ spider/apps/wechat.js
@@ -3,7 +3,8 @@ export default async () => {
   var url = "https://pc.weixin.qq.com/";
   var res = await Tools.requestGet(url);
   var dom = Tools.dom(res);
-  var version = dom(".download-text").html().split(" ").pop();
+  var version_text = dom(".download-text").html();
+  var version = version_text.match(/\d+\.\d+\.\d+/)[0];
   var x64url = dom("#downloadButton").attr("href");
   return {
     name: "wechat",
