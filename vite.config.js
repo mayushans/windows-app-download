942aea912ad60293cc0d27aa48adf937a6b35851
MaYushan <levimacn@qq.com>
2026-01-28 01:47:59 +0800

    add vite-plugin-cdn-import

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cdn from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   "base": "",
   plugins: [
     vue(),
+    cdn({
+        modules: ['vue'],
+    }),
   ],
   resolve: {
     alias: {
